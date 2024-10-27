068028cb6203475468d7d95200e4c1b4deb481f6
Rajat Kumar <101243704+rajatkumar1797@users.noreply.github.com>
2024-10-27 19:42:10 +0000

    variables

---
+++ project/var.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "rajat@google.com"
+accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+//accountId = 2
+accountEmail = "suneel@g.com"
+accountPassword = "121212"
+accountCity = "Basti"
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
