e2bb59f6f3d27aa62aaeff6891d10e5e2efcfe6b
Rajat Kumar <101243704+rajatkumar1797@users.noreply.github.com>
2024-10-28 18:32:28 +0000

    conversions

--- project/conversionoperation.js
+++ project/conversionoperation.js
@@ -19,3 +19,13 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//************************************************ Operations   *************************************************************//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue); // to convert any value in negative.
+
+let str1 = "Rajat"
+let str2 =" KumaR"
+let str3 = str1+str2
+console.log(str3); // to add to strings
