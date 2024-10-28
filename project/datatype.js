84dba56b3e1a96c41eff9d18766b881b9c7e4003
Rajat Kumar <101243704+rajatkumar1797@users.noreply.github.com>
2024-10-28 17:49:21 +0000

    Datatypes

---
+++ project/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+// alert (3+3) // we are using node.js, not browser.
+console.log(3+3);
+console.log("Rajat");
+
+let name = "Rajat kumar" // string
+let age = 18 // number
+let isLoggedIn = False // boollean 
+
+// Primitive data types.
+
+//number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value (empty value mean there was a memory)
+// undefined => not giving any value. 
+// symbol => to define the uniqueness 
+
+//Object
+console.log(typeof age); // to find the type of any object. you can replace age with any other object to find the type of any object. 
+
+
+
