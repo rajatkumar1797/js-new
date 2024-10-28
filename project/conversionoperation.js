5a5bdc50c3c411994205136a38f4b9e1b817dbc4
Rajat Kumar <101243704+rajatkumar1797@users.noreply.github.com>
2024-10-28 18:07:19 +0000

    conversion

---
+++ project/conversionoperation.js
@@ -0,0 +1,21 @@
+let score = 33
+console.log(typeof score); // to find the type of score weather it was num,string or a boollean. 
+let valueInNumber = Number(score) // to convert the any object in number
+console.log(valueInNumber)
+
+// "33" => 33
+//"33abc" => NaN
+// true =>1; false => 0
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false.
+// "" => false
+// "Rajat" => true
+ 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
